1aa3eba7c992be18f1777d1ad3426c1ecb3247ae
KARTIK RANA <kartikrana3332@gmail.com>
2025-05-09 02:47:53 +0530

    added db.js,using cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
-
+import dotenv from 'dotenv';
+import cors from 'cors';
 const app = express();
+app.use(cors());
+app.use(express.json());
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
