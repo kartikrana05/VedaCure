9e62744597fce78d157ab90c9dd2b4741e98da9b
KARTIK RANA <kartikrana3332@gmail.com>
2025-05-09 02:50:36 +0530

    dotenv installed, fixing Port

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
